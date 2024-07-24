14b23daa5666a95bf3b37c261bc3c4e1de4f5ab2
darshu1911 <141736790+darshu1911@users.noreply.github.com>
2024-07-24 18:26:45 +0530

    Update

--- cypress/e2e/First Project/Login.cy.js
+++ cypress/e2e/First Project/Login.cy.js
@@ -6,16 +6,17 @@ describe('Login Application', () => {
 
         cy.get("input[value='Log In']").click()
        
+        // cy.get('tbody > :nth-child(1) > :nth-child(1) > a').click()
         // cy.get(".smallText").should('have.text','Welcome Darshan Darsh')
        
     })
 
     it('Checking Account Details', () => {
 
-        // cy.get('tbody > :nth-child(1) > :nth-child(1) > a').click()
-        // cy.get('#leftPanel > ul > :nth-child(1) > a').click()
-        cy.get("a[href='openaccount.htm']").click()
-        cy.get('#accountDetails > .title')
+        cy.get('tbody > :nth-child(1) > :nth-child(1) > a').click()
+        cy.get('#leftPanel > ul > :nth-child(1) > a').click()
+    //     // cy.get("a[href='openaccount.htm']").click()
+    //     // cy.get('#accountDetails > .title')
 
     })  
 
